7a49dcfc17392995f85d66544b72b74ca2920428
Deepeshac2890 <deepeshac290@gmail.com>
2022-06-17 00:54:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -0,0 +1,93 @@
+package com.example.jettrivia.component
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jettrivia.screens.QuestionsViewModel
+import com.example.jettrivia.utils.AppColors
+
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    if (viewModel.data.value.loading == true) {
+
+    } else {
+        QuestionDisplay()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp),
+        color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+            DrawDottedLine(pathEffect)
+        }
+    }
+}
+
+@Composable
+fun QuestionTracker(counter: Int = 10, totalCount: Int = 100) {
+    Text(text = buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                color = AppColors.mLightGray,
+                fontWeight = FontWeight.Bold,
+                fontSize = 27.sp
+            )
+        ) {
+            append("Question $counter/")
+        }
+        withStyle(
+            style = SpanStyle(
+                color = AppColors.mLightGray,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp
+            )
+        ) {
+            append(totalCount.toString())
+        }
+    }, modifier = Modifier.padding(20.dp))
+}
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(x = 0f, y = 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+    }
+}
---
+++ app/src/main/java/com/example/jettrivia/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.jettrivia.utils
+
+object Constants {
+    // world.json -> is the path
+    const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/"
+}
